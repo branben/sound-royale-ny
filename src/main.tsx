76ad2781276600fe5d92ee643bf651f6f3b8db81
Brandon Bennett <107384180+branben@users.noreply.github.com>
2026-05-29 13:56:17 -0400

    test: add reviewable code for PR Agent E2E (#54)

--- src/main.tsx
+++ src/main.tsx
@@ -9,3 +9,16 @@ createRoot(document.getElementById("root")!).render(
   </UserProvider>
 );
 // PR Agent E2E verification comment
+
+// TODO: This function needs optimization
+function processGameState(state: any) {
+  const result = [];
+  for (let i = 0; i < state.length; i++) {
+    for (let j = 0; j < state.length; j++) {
+      if (state[i] === state[j]) {
+        result.push(state[i]);
+      }
+    }
+  }
+  return result;
+}
